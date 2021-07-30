7ab2f5833c3fc5dd8a0a430574a3b6533694befe
Sabarish Nadarajan <sabarish.nadarajan@citrix.com>
2021-07-29 22:17:53 -0400

    feat: Successful GET request

--- src/EventBrite/query.ts
+++ src/EventBrite/query.ts
@@ -1,13 +1,21 @@
 
 import { ebEnvironment } from "../Environment/environment";
 import fetch from 'node-fetch';
+
+/** Needs to be a user input */
 const startDateInit = '2021-07-20';
 const startDateEnd =  '2021-07-25';
+/**  */
+
 const eventPath =  `/v3/series/117176613405/events?start_date.range_start=${startDateInit}&start_date.range_end=${startDateEnd}`;
 const eventAPI = ebEnvironment.base + eventPath
 
 
 export async function getEventDetails(){
-    const response = await fetch(eventAPI);
-    return response;
+    const response = await fetch(eventAPI,{
+        method:'GET',
+        headers:{'Content-type': 'application/json', 'Authorization': 'Bearer YU3ZOA2QWNF4R3SZFGAP'}
+
+    })
+    return response.json();
 }
