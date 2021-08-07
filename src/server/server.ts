01199094edba9de433d267ea07ad10df503d52f7
Sabarish Nadarajan <sabarish.nadarajan@citrix.com>
2021-08-07 09:53:15 -0400

    feat: add port explicit

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import {start} from '../index'
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 start();
 
@@ -10,7 +10,7 @@ app.get('/',(req,res)=>{
     res.send('Heroku');
 })
 
-
+console.log('Port = ', port);
 app.listen(port,()=>{
     console.log(`Running on ${port}`);
 })
